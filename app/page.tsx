2e8cafa00ed5410ab057a89f7616eb371279ec60
Thomas A <99028957+Silinia@users.noreply.github.com>
2025-03-02 10:39:30 +0100

    add counter feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import CounterModule from "../app/components/CounterModule";
+
 export const metadata = {
   title: "App Router",
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
-}
+  return (
+  <div className="font-family: sans-serif">
+      <h1>Page d'accueil</h1>
+      <p>Module Counter :</p>
+      <div>
+        <CounterModule initialValue={5} />
+      </div>
+    </div>
+)}
